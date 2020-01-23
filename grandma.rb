3cbdab189995838943b3ac872ad8812625f2ef9a
Tammy Lam <lamtammy1515@gmail.com>
2020-01-23 00:07:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speaking_to_grandma(phrase)
+def speak_to_grandma(phrase)
   phrase = "I LOVE YOU GRANDMA!"
   if phrase == "I LOVE YOU GRANDMA!"
     return " I LOVE YOU TOO PUMPKIN!"
